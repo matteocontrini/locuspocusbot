c7072f106c26892bce1fc91621a8a2111d81583d
Matteo <m.contrini@gmail.com>
2017-10-19 21:25:43 +0200

    moved tg as an external dependency

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/matteocontrini/locuspocusbot/tg"
+	"github.com/matteocontrini/go-tg"
 	"github.com/pelletier/go-toml"
 )
 
---
+++ vendor/github.com/matteocontrini/go-tg/README.md
@@ -0,0 +1,3 @@
+# go-tg
+
+See [locuspocusbot](https://github.com/matteocontrini/locuspocusbot) for a usage example.
